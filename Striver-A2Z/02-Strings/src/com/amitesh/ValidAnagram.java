0493585647a7d5b5ad4f302222f0acbdefa3e74d
amitesh08 <aamitesh125@gmail.com>
2025-08-04 08:05:38 +0530

    Strings - valid Anagrams

---
+++ Striver-A2Z/02-Strings/src/com/amitesh/ValidAnagram.java
@@ -0,0 +1,23 @@
+package com.amitesh;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram"; String t = "nagaran";
+        System.out.println(isAnagram(s,t));
+    }
+    public static boolean isAnagram(String s, String t){
+        if (s.length() != t.length()) return false;
+
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) freq[c - 'a']++;
+        for(char c : t.toCharArray()) freq[c - 'a']--;
+
+        for(int f : freq){
+            if(f != 0) return false;
+
+        }
+        return true;
+    }
+}
