65f146bd74fc714aba8f589b17827d260acaac74
amitesh08 <aamitesh125@gmail.com>
2025-09-23 09:05:19 +0530

    optimised fibonacci

--- Striver-A2Z/05-Recursion/src/com/amitesh/Fibonacci.java
+++ Striver-A2Z/05-Recursion/src/com/amitesh/Fibonacci.java
@@ -4,7 +4,7 @@ public class Fibonacci {
     public static void main(String[] args) {
         //Q. 509. Fibonacci Number
         int n = 50;
-        System.out.println(fib(n));
+        System.out.println(fiboFormula(n));
     }
     static int fib(int n){
         /*
@@ -14,5 +14,10 @@ public class Fibonacci {
         if(n < 2) return n;
 
         return fib(n-1) + fib(n-2);
+    }
+    static long fiboFormula(int n){
+        // just for demo , use long instead.
+        long i = (long) (Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
+        return i;
     }
 }
